5116960ad8b19e3d37cd15d90e6698ff2e7b08f6
Daniel Clarke <daniel.clarke@DESKTOP-278084.bbtravel.local>
2019-11-29 10:58:52 +0000

    neaten up the users admin blade file with a foundation table and add a edit and delete button

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,12 +7,29 @@
     <div class="row">
         <div class="large-12 columns">
             <h1>Users</h1>
-            <ul>
-                @foreach($users as $user)
-                {{ $user->name }} - {{ $user->email }}
-                @endforeach
-            </ul>
-            <br/>
+            <table >
+                <thead>
+                    <tr>
+                        <th width="200">Users id</th>
+                        <th>Users Name</th>
+                        <th>Users Email</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>
+                                <a href="{{ route('admin.users.edit', $user->id) }}" class="success button">Edit</a>
+                                <a href="{{ route('admin.users.destroy', $user->id) }}" class="alert button">Delete</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
