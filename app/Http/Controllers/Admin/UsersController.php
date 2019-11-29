e5341cca0869a15dd357652486fb0948bb22a1a1
Daniel Clarke <daniel.clarke@DESKTOP-278084.bbtravel.local>
2019-11-29 12:12:48 +0000

    Add methods to the user model to return true if they have a specific user role, then created a Service Provider with a gate for delete and update users and finally use the gates in the user controller

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\User;
 use App\Role;
+use Gate;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -36,8 +37,11 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
-        $roles = Role::all();
+        if(Gate::denies('edit-users')){
+            return redirect()->route('admin.users.index');
+        }
 
+        $roles = Role::all();
         return view('admin.users.edit')->with([
             'user' => $user,
             'roles' => $roles
@@ -65,6 +69,10 @@ class UsersController extends Controller
      */
     public function destroy(User $user)
     {
+
+        if(Gate::denies('delete-users')){
+            return redirect()->route('admin.users.index');
+        }
         $user->roles()->detach();
         $user->delete();
         return redirect()->route('admin.users.index');
